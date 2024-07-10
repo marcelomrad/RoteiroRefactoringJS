49a63e66fe67294b221263b1369a0b4ba2e54bf1
marcelomrad <97288064+marcelomrad@users.noreply.github.com>
2024-07-10 17:38:18 -0300

    Commit 2 - Replace Temp With Query

--- index.js
+++ index.js
@@ -1,28 +1,33 @@
 const { readFileSync } = require('fs');
 
-function calcularTotalApresenteacao(apresentacao, peca) {
+function getPeca(apresentacao){
+  return pecas[apresentacao.id];
+}
+
+function calcularTotalApresenteacao(apre) {
   let total = 0;
-  switch (peca.tipo) {
+  switch (getPeca(apre).tipo) {
     case "tragedia":
       total = 40000;
-      if (apresentacao.audiencia > 30) {
-        total += 1000 * (apresentacao.audiencia - 30);
+      if (apre.audiencia > 30) {
+        total += 1000 * (apre.audiencia - 30);
       }
       break;
     case "comedia":
       total = 30000;
-      if (apresentacao.audiencia > 20) {
-        total += 10000 + 500 * (apresentacao.audiencia - 20);
+      if (apre.audiencia > 20) {
+        total += 10000 + 500 * (apre.audiencia - 20);
       }
-      total += 300 * apresentacao.audiencia;
+      total += 300 * apre.audiencia;
       break;
     default:
-      throw new Error(`Peça desconhecia: ${peca.tipo}`);
+      throw new Error(`Peça desconhecia: ${getPeca(apre).tipo}`);
   }
   return total;
 }
 
-function gerarFaturaStr (fatura, pecas) {
+
+function gerarFaturaStr (fatura) {
     let totalFatura = 0;
     let creditos = 0;
     let faturaStr = `Fatura ${fatura.cliente}\n`;
@@ -31,20 +36,20 @@ function gerarFaturaStr (fatura, pecas) {
                             minimumFractionDigits: 2 }).format;
   
     for (let apre of fatura.apresentacoes) {
-      const peca = pecas[apre.id];
+
       let total = 0;
 
       // calcula o total da apresentação
-      total = calcularTotalApresenteacao(apre, peca);
+      total = calcularTotalApresenteacao(apre);
 
   
       // créditos para próximas contratações
       creditos += Math.max(apre.audiencia - 30, 0);
-      if (peca.tipo === "comedia") 
+      if (getPeca(apre).tipo === "comedia") 
          creditos += Math.floor(apre.audiencia / 5);
   
       // mais uma linha da fatura
-      faturaStr += `  ${peca.nome}: ${formato(total/100)} (${apre.audiencia} assentos)\n`;
+      faturaStr += `  ${getPeca(apre).nome}: ${formato(total/100)} (${apre.audiencia} assentos)\n`;
       totalFatura += total;
     }
     faturaStr += `Valor total: ${formato(totalFatura/100)}\n`;
